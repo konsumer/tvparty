8ed99bd9adc8e083ad4e4eed5b7baa606db043d5
David Konsumer <konsumer@jetboystudio.com>
2013-04-16 15:31:20 -0700

    better logs

--- tvparty.init.sh
+++ tvparty.init.sh
@@ -8,7 +8,7 @@ INSTALL_DIR="/usr/local/share/tvparty"
 
 case "$1" in
   start)
-  exec "${INSTALL_DIR}/node_modules/.bin/forever" --sourceDir="${INSTALL_DIR}" -p /var/run app.js
+  exec "${INSTALL_DIR}/node_modules/.bin/forever" --sourceDir="${INSTALL_DIR}" -a -p /var/run -l /var/log/tvparty-daemon.log -o /var/log/tvparty-out.log -e /var/log/tvparty-err.log app.js
   ;;
 stop)
   exec "${INSTALL_DIR}/node_modules/.bin/forever" stop --sourceDir="${INSTALL_DIR}" app.js
