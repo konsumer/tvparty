abbd321ec6730f5f1589a79d54bb604636abfacd
David Konsumer <konsumer@jetboystudio.com>
2013-05-02 11:53:39 -0700

    / ‘getShows.js’

--- getShows.js
+++ getShows.js
@@ -3,22 +3,10 @@
  * It just grabs a list of supported shows from showRSS
  */
 
-var request = require('request'),
-	cheerio = require('cheerio'),
+var shows = require('./shows.js'),
 	fs = require('fs'),
-	path = require('path'),
-	shows=[];
+	path = require('path');
 
-request('http://showrss.karmorra.info/?cs=feeds', function(err, res, body){
-	var $ = cheerio.load(body, {ignoreWhitespace: true});
-	$('select[name=show] option').each(function(i, el){
-		shows.push({
-			id: $(this).attr('value'),
-			name: $(this).text()
-		});
-	});
-});
-
-process.on('exit', function(){
-	fs.writeFileSync(path.join(__dirname, 'shows.json'), JSON.stringify(shows, null, 4));
+shows.getShows(function(err, shows){
+	if 
 });
