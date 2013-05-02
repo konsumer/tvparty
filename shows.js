f194c6f46929b0705c42b681fc8e17d35fe37b46
David Konsumer <konsumer@jetboystudio.com>
2013-05-02 11:48:42 -0700

    / ‘shows.js’

--- shows.js
+++ shows.js
@@ -1,11 +1,30 @@
 
 /*
 
-Test parsing of tv shows
+Library to interact with showRSS
 
  */
 
-
-
 var request = require('request'),
-	shows = require('./shows.json');
+	cheerio = require('cheerio'),
+	fs = require('fs'),
+	path = require('path');
+
+/**
+ * Get list of shows
+ */
+exports.getShows = function(callback){
+	callback = callback || function(err, shows){};
+	request('http://showrss.karmorra.info/?cs=feeds', function(err, res, body){
+		if (err) return callback(err);
+		var shows = [];
+		var $ = cheerio.load(body, {ignoreWhitespace: true});
+		$('select[name=show] option').each(function(i, el){
+			shows.push({
+				id: $(this).attr('value'),
+				name: $(this).text()
+			});
+		});
+		return callback(null, shows);
+	});
+}
