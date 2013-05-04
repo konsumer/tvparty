c39f05d04c707ab58306ba011d89cd5bab3123ca
David Konsumer <konsumer@jetboystudio.com>
2013-05-04 16:48:28 -0700

    / ‘test.js’

--- test.js
+++ test.js
@@ -3,38 +3,29 @@ var kat=require('./providers/kat.js');
 /*
 kat.all()
 	.on('show', function(show){
-		console.log(show);
+		console.log('SHOW', show);
 	})
 	.on('end', function(){
 		console.log('got shows');
 	});
-*/
+
 
 kat.show('game-of-thrones-tv24493')
 	.on('episode', function(episode){
-		console.log(episode);
+		console.log('EPISODE', episode);
 	})
 	.on('end', function(){
 		console.log('got episodes');
 	});
 
-/*
-kat.show('game-of-thrones-tv24493', function(episode){
-	console.log("EPISODE", episode);
-});
 */
 
-/*
 var torrents = [];
-kat.torrents('266404348', function(torrent){
-	console.log(torrent);
-	torrents.push(torrent);
-});
-
-process.on('exit', function(){
-	kat.best(torrents, function(best){
-		console.log('BEST', best);
+kat.torrents()
+	.on('torrent', function(torrent){
+		console.log('TORRENT', torrent);
+		torrents.push(torrent);
+	})
+	.on('end', function(){
+		console.log('BEST', kat.best(torrents));
 	});
-});
-
-*/
