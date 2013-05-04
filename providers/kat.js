742c6fb7284461ead6a99f2d108a5a46a848c912
David Konsumer <konsumer@jetboystudio.com>
2013-05-04 15:15:45 -0700

    / ‘providers/kat.js’

--- providers/kat.js
+++ providers/kat.js
@@ -69,13 +69,13 @@ exports.show = function(id, callback){
 };
 
 /**
- * Get more specific info for a single episode
- * @param  {String}   id       The ID from list()
- * @param  {Function} callback (episode) - episode is info for one episode (called many times)
+ * Get list of torrents for an episode
+ * @param  {String}   id       The ID from show()
+ * @param  {Function} callback (torrents) - list of all torrents, & "best" torrent for this show
  */
-exports.episode = function(episode, callback){
-	get('http://kat.ph/media/getepisode/' + episode.id + '/', function($){
-		episode.torrents = [];
+exports.torrents = function(id, callback){
+	get('http://kat.ph/media/getepisode/' + id + '/', function($){
+		var torrents = [];
 		$('tr.odd, tr.even').each(function(j, el){
 			var torrent = {
 				name: $('.torrentname .font12px', this).text(),
@@ -83,7 +83,6 @@ exports.episode = function(episode, callback){
 				magnet: $('a.imagnet', this).attr('href'),
 				size: $('td.center', this).slice(0,1).text(),
 				files: $('td.center', this).slice(1,2).text(),
-				age: $('td.center', this).slice(2,3).text(),
 				seed: $('td.center', this).slice(3,4).text(),
 				leech: $('td.center', this).slice(4,5).text(),
 			};
@@ -91,13 +90,13 @@ exports.episode = function(episode, callback){
 			// normalize
 
 			var num = parseFloat(torrent.size);
-			if (torrent.size.indexOf('TB')!=-1){
+			if (torrent.size.indexOf('TB') !== -1){
 				torrent.size = num * 1024 * 1024 * 1024 * 1024;
-			}else if (torrent.size.indexOf('GB')!=-1){
+			}else if (torrent.size.indexOf('GB') !== -1){
 				torrent.size = num * 1024 * 1024 * 1024;
-			}else if (torrent.size.indexOf('MB')!=-1){
+			}else if (torrent.size.indexOf('MB') !== -1){
 				torrent.size = num * 1024 * 1024;
-			}else if (torrent.size.indexOf('KB')!=-1){
+			}else if (torrent.size.indexOf('KB') !== -1){
 				torrent.size = num * 1024;
 			}else{
 				torrent.size = num;
@@ -107,9 +106,9 @@ exports.episode = function(episode, callback){
 			torrent.seed = parseInt(torrent.seed, 10);
 			torrent.leech = parseInt(torrent.leech, 10);
 
-			episode.torrents.push(torrent);
+			torrents.push(torrent);
 		});
-		callback(episode);
+		callback(torrents);
 	});
 };
 
