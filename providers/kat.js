4f28b6c6dc331c05c7150656224ae7cc283fc605
David Konsumer <konsumer@jetboystudio.com>
2013-05-04 14:02:02 -0700

    / ‘providers/kat.js’

--- providers/kat.js
+++ providers/kat.js
@@ -63,7 +63,7 @@ exports.get = function(id, callback){
 						var torrent = {
 							name: $('.torrentname .font12px', this).text(),
 							link: 'http://kat.ph' + $('.torrentname .font12px', this).attr('href'),
-							size: $(this).find('.center:nth-child(1)').text(),
+							size: $('td.center:nth-child(1)', this).text(),
 						};
 						episode.torrents.push(torrent);
 					});
