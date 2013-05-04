c06dd5aa1e14c21c3f9e3f4eb5684d6aa6c5a5da
David Konsumer <konsumer@jetboystudio.com>
2013-05-04 15:06:40 -0700

    / ‘providers/kat.js’

--- providers/kat.js
+++ providers/kat.js
@@ -27,7 +27,7 @@ var get = function(u, callback){
  * Get list of shows
  * @param  {Function} callback (shows) - shows is array of info
 */
-exports.list = function(callback){
+exports.all = function(callback){
 	callback = callback || function(shows){};
 	get('http://kat.ph/tv/show/', function($){
 		var shows=[];
@@ -49,7 +49,7 @@ exports.list = function(callback){
  * @param  {String}   id       The ID from list()
  * @param  {Function} callback (episode) - episode is info for one episode (called many times)
  */
-exports.get = function(id, callback){
+exports.show = function(id, callback){
 	get('http://kat.ph/' + id + '/', function($){
 		$('a.infoListCut').each(function(i, el){
 			var episode = {
@@ -59,43 +59,6 @@ exports.get = function(id, callback){
 			};
 			if ($(this).attr('onclick')){
 				episode.id = $(this).attr('onclick').match(/\d+/)[0];
-				get('http://kat.ph/media/getepisode/' + episode.id + '/', function($){
-					episode.torrents = [];
-					$('tr.odd, tr.even').each(function(j, el){
-						var torrent = {
-							name: $('.torrentname .font12px', this).text(),
-							link: 'http://kat.ph' + $('.torrentname .font12px', this).attr('href'),
-							magnet: $('a.imagnet', this).attr('href'),
-							size: $('td.center', this).slice(0,1).text(),
-							files: $('td.center', this).slice(1,2).text(),
-							age: $('td.center', this).slice(2,3).text(),
-							seed: $('td.center', this).slice(3,4).text(),
-							leech: $('td.center', this).slice(4,5).text(),
-						};
-
-						// normalize
-
-						var num = parseFloat(torrent.size);
-						if (torrent.size.indexOf('TB')!=-1){
-							torrent.size = num * 1024 * 1024 * 1024 * 1024;
-						}else if (torrent.size.indexOf('GB')!=-1){
-							torrent.size = num * 1024 * 1024 * 1024;
-						}else if (torrent.size.indexOf('MB')!=-1){
-							torrent.size = num * 1024 * 1024;
-						}else if (torrent.size.indexOf('KB')!=-1){
-							torrent.size = num * 1024;
-						}else{
-							torrent.size = num;
-						}
-
-						torrent.files = parseInt(torrent.files, 10);
-						torrent.seed = parseInt(torrent.seed, 10);
-						torrent.leech = parseInt(torrent.leech, 10);
-
-						episode.torrents.push(torrent);
-					});
-					callback(episode);
-				});
 			}else{
 				callback(episode);
 			}
@@ -103,4 +66,48 @@ exports.get = function(id, callback){
 	});
 };
 
+/**
+ * Get more specific info for a single episode
+ * @param  {String}   id       The ID from list()
+ * @param  {Function} callback (episode) - episode is info for one episode (called many times)
+ */
+exports.episode = function(episode, callback){
+	get('http://kat.ph/media/getepisode/' + episode.id + '/', function($){
+		episode.torrents = [];
+		$('tr.odd, tr.even').each(function(j, el){
+			var torrent = {
+				name: $('.torrentname .font12px', this).text(),
+				link: 'http://kat.ph' + $('.torrentname .font12px', this).attr('href'),
+				magnet: $('a.imagnet', this).attr('href'),
+				size: $('td.center', this).slice(0,1).text(),
+				files: $('td.center', this).slice(1,2).text(),
+				age: $('td.center', this).slice(2,3).text(),
+				seed: $('td.center', this).slice(3,4).text(),
+				leech: $('td.center', this).slice(4,5).text(),
+			};
+
+			// normalize
+
+			var num = parseFloat(torrent.size);
+			if (torrent.size.indexOf('TB')!=-1){
+				torrent.size = num * 1024 * 1024 * 1024 * 1024;
+			}else if (torrent.size.indexOf('GB')!=-1){
+				torrent.size = num * 1024 * 1024 * 1024;
+			}else if (torrent.size.indexOf('MB')!=-1){
+				torrent.size = num * 1024 * 1024;
+			}else if (torrent.size.indexOf('KB')!=-1){
+				torrent.size = num * 1024;
+			}else{
+				torrent.size = num;
+			}
+
+			torrent.files = parseInt(torrent.files, 10);
+			torrent.seed = parseInt(torrent.seed, 10);
+			torrent.leech = parseInt(torrent.leech, 10);
+
+			episode.torrents.push(torrent);
+		});
+		callback(episode);
+	});
+};
 
