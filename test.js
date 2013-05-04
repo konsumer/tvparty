5db6033c77256f9b57bcf7d06a9eb5ef85422962
David Konsumer <konsumer@jetboystudio.com>
2013-05-04 12:26:20 -0700

    / ‘test.js’

--- test.js
+++ test.js
@@ -1,5 +1,13 @@
 var kat=require('./providers/kat.js');
 
+/*
+
 kat.list(function(shows){
 	console.log(shows);
+});
+
+*/
+
+kat.get('game-of-thrones-tv24493', function(episodes){
+	console.log(episodes);
 });
