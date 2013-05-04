a88885d485ed317fb8a1b32b93cfbef4c9406f50
David Konsumer <konsumer@jetboystudio.com>
2013-05-04 12:52:57 -0700

    / ‘providers/kat.js’

--- providers/kat.js
+++ providers/kat.js
@@ -60,7 +60,18 @@ exports.get = function(id, callback){
 					};
 					if ($(el).attr('onclick')){
 						episode.id = $(el).attr('onclick').match(/\d+/)[0];
+						http.get({hostname:'kat.ph', path: '/media/getepisode/' + episode.id + '/'}, function(res) {
+							var chunks = [];
+							res.pipe(zlib.createGunzip())
+								.on('data', function (data) { chunks.push(data); })
+								.on('end', function(){
+									var data = JSON.parse(Buffer.concat(chunks)+"");
+									var $ = cheerio.load(data.html, {ignoreWhitespace: true});
+									console.log(data.html);
+								});
+						});
 					}
+
 					episodes.push(episode);
 				});
 				callback(episodes);
