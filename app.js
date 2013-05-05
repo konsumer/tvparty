512398459271736392ea8eb4dc5026deb45197e4
David Konsumer <konsumer@jetboystudio.com>
2013-05-05 11:54:01 -0700

    / ‘app.js’

--- app.js
+++ app.js
@@ -160,6 +160,7 @@ app.get('/shows', function(req, res){
 // get detailed info about a show
 app.get('/show/:id', function(req, res){
 	var info = getShow(req.params.id);
+	console.log(info);
 	provider.show(req.params.id)
 		.on('episode', function(episode){
 			info = Object.create(info, episode);
