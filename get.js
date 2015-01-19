f4034453a283430e822dc8542956e2e5dfb5f05a
David Konsumer <konsumer@jetboystudio.com>
2015-01-18 22:46:06 -0800

    seperate get

---
+++ get.js
@@ -0,0 +1,14 @@
+var	ajax = require('superagent'),
+	cheerio = require('cheerio');
+
+/**
+ * Shortcut for HTTP GET + cheerio-parse
+ * 
+ * @param  {String}   u        Full URL
+ * @param  {Function} callback called with cheerio pseudo-jquery object for page
+ */
+module.exports = function(u, callback){
+	ajax.get(u, function(error, res){
+		callback(cheerio.load(res.body, {ignoreWhitespace: true}));
+	});
+};
